9f84267ef0a3475c4e788a3f34d1942e0a398e0e
Ben Lucke <benlucke@hotmail.com>
2024-09-16 23:56:39 -0500

    add solution to part b

--- projects/1p/p1_668.m
+++ projects/1p/p1_668.m
@@ -63,13 +63,12 @@ n_r = trim.dynamic_pressure * wing.area * (wing.span.^2) ...
     * stab_derivatives.cnr / (2 * full.inertia_z * trim.airspeed);
 
 
-%%% C Matrix Stabiltiy Derivatives
+%%% B Matrix Stabiltiy Derivatives
 % Anonymous function to compute integrand of C_l_da
 cl_da_integrand = @(y) ((2*wing.coef_lift_slope*wing.ail_tau*wing.root_chord)...
     /(wing.area * wing.span)) * ((y.^2/2) ...
     + (((wing.tip_chord/wing.root_chord) - 1)/(wing.span/2)) * (y.^3/3));
 
-%%% C Matrix Stability Derivatives
 stab_derivatives.cy_dr = vtail.area / wing.area * vtail.rudder_tau ...
     * vtail.coef_lift_slope;
 stab_derivatives.cl_da = cl_da_integrand(wing.ail_outer) - cl_da_integrand(wing.ail_inner);
@@ -78,7 +77,7 @@ stab_derivatives.cn_da = 2 * wing.ail_factor * trim.coef_lift * stab_derivatives
 stab_derivatives.cn_dr = -vtail.eff * vtail.volume_ratio * vtail.coef_lift_slope * vtail.rudder_tau;
 
 
-%%% C Matrix Coefficients
+%%% B Matrix Coefficients
 y_dr = trim.dynamic_pressure * wing.area / full.mass ...
     * stab_derivatives.cy_dr;
 
@@ -95,10 +94,46 @@ n_dr = trim.dynamic_pressure * wing.area * wing.span * stab_derivatives.cn_dr / 
 A_lat = [y_b/trim.airspeed, y_p/trim.airspeed, y_r/trim.airspeed - 1, trim.gravity/trim.airspeed*cos(trim.flight_path);
     l_b, l_p, l_r, 0;
     n_b, n_p, n_r, 0;
-    0, 1, 0, 0]
+    0, 1, 0, 0];
 
-C_lat = [0, y_dr/trim.airspeed;
+B_lat = [0, y_dr/trim.airspeed;
     l_da, l_dr;
     n_da, n_dr;
-    0, 0]
+    0, 0];
 
+C_lat = [0 0 1 0
+    0 0 0 1];
+
+D_lat = [0 0
+    0 0];
+
+lat_sys = ss(A_lat, B_lat, C_lat, D_lat);
+lat_sys.stateName = {'beta', 'p', 'r', 'phi'};
+lat_sys.inputName = {'delta aileron', 'delta rudder'};
+lat_sys.outputName = {'\Delta r','\Delta\phi'};
+
+%% Part b - Compute and Display Damping of Aircraft Modes
+[wn, zeta, p] = damp(lat_sys);
+fprintf('___________________________________________________________')
+fprintf('\nSpiral Mode')
+fprintf('\n\nEigenvalue: %d', p(1))
+fprintf('\nNatural Frequency: %f rad/sec', wn(1))
+fprintf('\nDamping Ratio: N/A')
+fprintf('\n')
+
+fprintf('\n___________________________________________________________')
+fprintf('\nRoll Subsistence Mode')
+fprintf('\n\nEigenvalue: %d', p(2))
+fprintf('\nNatural Frequency: %f rad/sec', wn(2))
+fprintf('\nDamping Ratio: N/A')
+fprintf('\n')
+
+fprintf('\n___________________________________________________________')
+fprintf('\nDutch Roll Mode')
+fprintf('\n\nEigenvalues: %f%+fj, %f%-fj', real(p(3)), imag(p(3)), real(p(4)), imag(p(4)))
+fprintf('\nNatural Frequency: %f rad/sec', wn(3))
+fprintf('\nDamping Ratio: %f', zeta(3))
+fprintf('\n')
+
+fprintf(['\nThe aircraft is laterally-directionally unstable due to the\n' ...
+    'eigenvalue corresponding with the spiral mode lying in the RHP.\n'])
