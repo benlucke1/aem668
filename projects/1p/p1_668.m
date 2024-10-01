8b4bc543ddd9855f339c845596becb01f649be75
Ben Lucke <benlucke@hotmail.com>
2024-09-30 23:47:21 -0500

    add complete project 1

--- projects/1p/p1_668.m
+++ projects/1p/p1_668.m
@@ -7,7 +7,6 @@
 %% Housekeeping
 clear, clc, close all, format compact
 run('project1_parameters.m')
-
 %% Part a - Compute and Output Estimated Stability Derivatives
 %%% A Matrix Stability Derivatives
 stab_derivatives.cyb = -vtail.eff * vtail.area / wing.area ...
@@ -101,16 +100,14 @@ B_lat = [0, y_dr/trim.airspeed;
     n_da, n_dr;
     0, 0];
 
-C_lat = [0 0 1 0
-    0 0 0 1];
+C_lat = [0 0 1 0;
+    0 0  0 1];
 
-D_lat = [0 0
-    0 0];
-
-lat_sys = ss(A_lat, B_lat, C_lat, D_lat);
+lat_sys = ss(A_lat, B_lat, C_lat, 0);
 lat_sys.stateName = {'beta', 'p', 'r', 'phi'};
-lat_sys.inputName = {'delta aileron', 'delta rudder'};
-lat_sys.outputName = {'\Delta r','\Delta\phi'};
+lat_sys.inputName = {'delta_aileron', 'delta_rudder'};
+lat_sys.outputName = {'rdot','phidot'};
+size(lat_sys)
 
 %% Part b - Compute and Display Damping of Aircraft Modes
 [wn, zeta, p] = damp(lat_sys);
@@ -167,7 +164,7 @@ L1 = controller_1 * ol;
 % Compare Open Loop Plant to Requirements
 figure()
 bodemag(ol,'b', L1, 'b--', r2,'g', r3,'g', r4,'g', r1,'rx')
-title('Bode Magnitude of Plant & Requirements')
+title('Heading Hold Bode Magnitude of Plant & Requirements')
 legend('Uncompensated Plant','Plant with Proportional Boost'...
     ,'Requirements','Interpreter','Latex','FontSize',14)
 
@@ -179,7 +176,7 @@ L2 = ol * controller_2;
 % Compare low-frequency gain to requirements
 figure()
 bodemag(ol, 'b', L2,'b--', r2,'g', r3,'g', r4,'g', r1,'rx')
-title('Bode Magnitude of Plant & Requirements')
+title('Heading Hold Bode Magnitude of Plant & Requirements')
 legend('Uncompensated Plant','Plant with Proportional and Integral Boost'...
     ,'Requirements','Interpreter','Latex','FontSize',14)
 
@@ -191,7 +188,7 @@ controller_3 = controller_2 * rolloff_stage;
 L3 = ol * controller_3;
 figure()
 bodemag(ol, 'b', L3,'b--', r2,'g', r3,'g', r4,'g', r1,'rx')
-title('Bode Magnitude of Plant & Requirements')
+title('Heading Hold Bode Magnitude of Plant & Requirements')
 legend('Uncompensated Plant','Plant with Proportional, Integral Boost and Rolloff'...
     ,'Requirements','Interpreter','Latex','FontSize',14)
 closed_loop = feedback(controller_3, 1);
@@ -228,10 +225,111 @@ title('Outer Loop Tracking Error for u = $sin(.05 t)$','Interpreter',...
 ylim([-.1 .1])
 legend('Tracking Error', '5\% Bound','Interpreter','Latex',...
     'Fontsize', 14)
-
+hold off
 
 %%% Prove < 5% gain for frequencies above 10 rad/sec
 fprintf(['The magnitude of the open loop response at 10 radians per' ...
     ' second is %.2f dB.\n\n'], 20*log10(abs(freqresp(L3, 10))))
 
-%% Part d - Lateral-directional Inner Loop Controller
+%% Part d - Lateral-directional Inner Loop Controller 
+omega_w = 1.820497/10;
+tau_actuator = .01;
+omega_actuator = 1/tau_actuator;
+
+K_ari = tf(-n_da/n_dr, 'InputName','delta_ac','OutputName', 'delta_ari');
+
+A_a = 1/(s + omega_actuator);
+A_a.InputName = 'delta_ac';
+A_a.OutputName='delta_aileron';
+
+A_r = 1/(s + omega_actuator);
+A_r.InputName = 'delta_rc';
+A_r.OutputName='delta_rudder';
+
+K_r = -s/(s+(omega_w/5));
+K_r.InputName = 'rdot';
+K_r.OutputName = 'rdot_damped';
+
+lat_sys.stateName = {'betadot', 'pdot', 'rdot', 'phidot'};
+lat_sys.inputName = {'delta_aileron', 'delta_rudder'};
+lat_sys.outputName = {'rdot','phidot'};
+
+actuator_matrix = [A_a 0; 0 A_r];
+roll_ari = [1; K_ari];
+actuatorsPlusPlant = series(actuator_matrix, lat_sys);
+ol_sys = feedback(actuatorsPlusPlant, K_r, 2, 1) * roll_ari;
+roll_ol = tf(ol_sys(2));
+
+
+% Want crossover frequency at 10 radians per second
+bw = 10;
+r1 = frd(1,bw);
+
+% For 45 degrees phase margin, want -30 db/dec around crossover frequency
+mid_freq = linspace(bw/3, bw*3, 500);
+r2 = frd((bw./mid_freq).^(1.5), mid_freq);
+
+% For low frequency response up to 1 radians per sec,
+lf = linspace(1e-3, 1);
+r3 = frd(tf(1 + 1/.05), lf);
+
+% For high frequency response greater than 10 rad/sec < 5% gain,
+hf = linspace(100,1000);
+r4 = frd(tf(.05 / (1+.05)), hf);
+
+%%% Proportional Control Stage
+P_controller = tf(1/abs(freqresp(roll_ol, bw)));
+
+
+%%% Derivative control stage
+wd = 20;
+D_controller = tf(1/wd*s + 1);
+
+%%% Integral Control Stage
+I_controller = (s+1.820497/2)/s;
+
+%%% Fully compensated system
+PID_controller = P_controller*I_controller*D_controller;
+L = PID_controller * roll_ol;
+roll_cl = feedback(PID_controller * roll_ol, 1);
+
+
+%%% Print stuff for JDL 
+fprintf('The roll control controller TF is \n')
+PID_controller
+
+fprintf('The poles of the roll control inner loop loop are\n')
+pole(roll_cl)
+
+[Gm,Pm,Wcg,Wcp] = margin(roll_cl);
+fprintf(['The gain margin for the roll controller is infinite. The phase margin is ' ...
+    '%.2f degrees\nat a crossover frequency of %.2f rad/sec.\n\n'],Pm,Wcp)
+
+fprintf(['The gain at the desired loop bandwidth of %.2f rad/sec is %.4f dB. \nThe gain at ' ...
+    '100 rad/sec is %.4f dB.\n'], 11, 20*log10(abs(freqresp(L, 11))), ...
+    20*log10(abs(freqresp(L, 100))))
+
+%%% Prove 5% steady state error requirement met
+t = linspace(0,400,1000);
+input_frequency = 1; % rad/sec
+u = sin(input_frequency * t);
+y_out = lsim(roll_cl, u, t);
+e = zeros(1,numel(t));
+for i = 1:numel(t)
+    e(i) = y_out(i) - u(i);
+end
+figure()
+hold on
+plot(t,e)
+plot(t, .05*ones(length(t)),'r--', t, -.05*ones(length(t)),'r--')
+xlabel('Time (sec)')
+ylabel('Error')
+title('Inner Loop Tracking Error for u = $sin(t)$','Interpreter',...
+    'latex','FontSize',14)
+ylim([-.1 .1])
+legend('Tracking Error', '5\% Bound','Interpreter','Latex',...
+    'Fontsize', 14)
+hold off
+
+
+simulink_sim = sim('aem668_project1_simulink');
